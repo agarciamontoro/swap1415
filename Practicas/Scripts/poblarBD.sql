bb29d9e611f113430b476c578de0fab247e41bbe
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-05-17 17:53:41 +0200

    Añade script SQL de la quinta práctica

---
+++ Practicas/Scripts/poblarBD.sql
@@ -0,0 +1,5 @@
+CREATE TABLE datos(nombre VARCHAR(100),tlf INT);
+
+INSERT INTO datos(nombre,tlf) VALUES ("Pepe",95834987);
+INSERT INTO datos(nombre,tlf) VALUES ("Paca",95834985);
+INSERT INTO datos(nombre,tlf) VALUES ("Lola",95834984);
